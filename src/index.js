ab8646f75d254d4ab7b352f3c3e92ad185ffd2a1
Faith <faithhafen@gmail.com>
2021-01-26 16:26:34 -0700

    optimize urls

--- src/index.js
+++ src/index.js
@@ -2,17 +2,17 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import './index.css'
 import App from './App'
-import { BrowserRouter } from 'react-router-dom'
+import { HashRouter } from 'react-router-dom'
 import Navbar from './components/Navbar'
 import Footer from './components/Footer'
 
 ReactDOM.render(
   <React.StrictMode>
-    <BrowserRouter>
+    <HashRouter>
       <Navbar />
       <App />
       <Footer />
-    </BrowserRouter>
+    </HashRouter>
   </React.StrictMode>,
   document.getElementById('root')
 )
